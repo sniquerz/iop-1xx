66326538586b86bb1d05f15b952a6fc78af41149
rebeccahaughey <sniquerz@gmail.com>
2019-04-07 01:46:11 -0400

    added style.css to assets/css and included css for top navigation menu. linked assets/css/style.css to the index.html page.

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,43 +1,73 @@
 // JavaScript Document
 
+window.onload = init();
+
 function init() {
-  window.addEventListener('scroll', function(e) {
-    var distanceY = window.pageYOffset || document.documentElement.scrollTop,
-      shrinkOn = 300,
-      header = document.querySelector("header");
-    if (distanceY > shrinkOn) {
-      classie.add(header, "smaller");
-    } else {
-      if (classie.has(header, "smaller")) {
-        classie.remove(header, "smaller");
-      }
-    }
-  });
 
-  $.ajax({
-    method: 'GET',
-    url: 'assets/data/menu.json',
-    dataType: 'json',
-    success: function(data) {
-      console.log('all good');
-      console.log(data.menu.length);
-      console.log(data.menu);
+    window.addEventListener('scroll', function (e) {
+        var distanceY = window.pageYOffset || document.documentElement.scrollTop,
+            shrinkOn = 300,
+            header = document.querySelector("header");
+        if (distanceY > shrinkOn) {
+            classie.add(header, "smaller");
+        } else {
+            if (classie.has(header, "smaller")) {
+                classie.remove(header, "smaller");
+            }
+        }
+    });
 
-      if (data.menu.length > 0) {
 
-        data.menu.forEach(function(data) {
+    $.ajax({
+        method: 'GET',
+        url: 'assets/data/menu.json',
+        dataType: 'json',
+        success: function (data) {
 
-          console.log(data.MenuName);
-          console.log(data.MenuLink);
+            var menu = menuBuilder(data.menu);
 
-          $('nav').append('<a href="' + data.MenuLink + '">' + data.MenuName + '</a>');
-        });
-      }
-    },
-    error: function() {
-      console.log('all is not good');
-    }
-  })
+            $('nav').append(menu);
+
+        },
+        error: function () {
+
+            console.log('all is not good');
+
+        }
+    });
 
 }
-window.onload = init();
+
+
+function menuBuilder(obj) {
+
+    var theMenu = '';
+
+    if (obj.length > 0) {
+
+        theMenu = theMenu + '<ul>';
+
+        obj.forEach(function (item) {
+
+            theMenu = theMenu + '<li><a href="#">' + item.MenuName + '</a>';
+
+            if (item.Menus.length > 0) {
+
+                theMenu = theMenu + menuBuilder(item.Menus);
+
+            }
+
+            theMenu = theMenu + '</li>';
+
+        });
+
+        theMenu = theMenu + '</ul>';
+
+    } else {
+
+        console.log('no data');
+
+    }
+
+    return theMenu;
+}
