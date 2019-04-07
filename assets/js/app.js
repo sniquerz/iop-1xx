a8037ecf92086f22b7783645b542c28d1f5f8412
rebeccahaughey <sniquerz@gmail.com>
2019-04-07 11:12:07 -0400

    error corrections again

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,27 +1,10 @@
-// JavaScript Document
+// inside out project STEP-106
 
 window.onload = init();
 
 function init() {
 
-<<<<<<< HEAD
-  $.ajax({
-    method: 'GET',
-    url: 'assets/data/menu.json',
-    dataType: 'json',
-    success: function (data) {
-
-      var menu = menuBuilder(data.menu);
-
-        $('nav').append(menu);
-        
-    },
-    error: function() {
-      console.log('all is not good');
-    }
-  });
-=======
-    window.addEventListener('scroll', function (e) {
+    window.addEventListener('scroll', function(e) {
         var distanceY = window.pageYOffset || document.documentElement.scrollTop,
             shrinkOn = 300,
             header = document.querySelector("header");
@@ -34,86 +17,88 @@ function init() {
         }
     });
 
+
     $.ajax({
         method: 'GET',
-        url: 'assets/data/menu.json',
+        url: 'https://me.inside-out-project.com/wp-json/wp-api-menus/v2/menus/3',
+        dataType: 'json',
+        success: function(data) {
+            $('nav').hide();
+            var menu = menuBuilder(data.items);
+            $('nav').html(menu).slideDown();
+            $('nav li a').click(function() {
+                getPage($(this).data("pgid"));
+            });
+            getPage(314);
+            $("#loaderDiv").fadeOut("slow");
+        },
+        error: function() {
+            console.log('all is not good');
+        }
+    });
+
+
+    $.ajax({
+        method: 'GET',
+        url: 'https://me.inside-out-project.com/wp-json/wp-api-menus/v2/menus/12',
         dataType: 'json',
         success: function (data) {
-
-            var menu = menuBuilder(data.menu);
-
-            $('nav').append(menu);
-
+            var menu = menuBuilder(data.items, 'genLinks', 'footer-ul');
+            $('#genLinks').replaceWith(menu);
+            $('#genLinks li a').click(function () {
+                getPage($(this).data("pgid"));
+            });
         },
         error: function () {
-
             console.log('all is not good');
-
         }
     });
 
 }
 
-function menuBuilder(obj) {
-
+function menuBuilder(obj, targetEl, classInfo) {
     var theMenu = '';
-
     if (obj.length > 0) {
-
-        theMenu = theMenu + '<ul>';
-
+        let target = (targetEl)?' id="'+targetEl+'"':'';
+        let elClass = (classInfo)?' class="'+classInfo+'"':'';
+        theMenu = theMenu + '<ul'+target+''+elClass+'>';
+        console.log(theMenu+' '+target);
         obj.forEach(function (item) {
-
-            theMenu = theMenu + '<li><a href="#">' + item.MenuName + '</a>';
-
-            if (item.Menus.length > 0) {
-
-                theMenu = theMenu + menuBuilder(item.Menus);
-
+            theMenu = theMenu + '<li><a href="#" data-pgid="' + item.object_id + '">' + item.title + '</a>';
+            if (item.children) {
+                theMenu = theMenu + menuBuilder(item.children);
             }
-
             theMenu = theMenu + '</li>';
-
         });
-
         theMenu = theMenu + '</ul>';
-
     } else {
-
         console.log('no data');
-
     }
->>>>>>> 102-1
-
     return theMenu;
 }
-<<<<<<< HEAD
 
-function menuBuilder(obj) {
-
-  var theMenu = '';
-
-  if (obj.length > 0) {
-    theMenu = theMenu + '<ul>';
-      obj.forEach(function (item) {
-
-        theMenu = theMenu + '<li><a href="#">' + item.MenuName + '</a>';
-
-        if (item.Menus.length > 0) {
-              theMenu = theMenu  + menuBuilder(item.Menus);
+function getPage(obj) {
+    $("#loaderDiv").fadeIn("slow");
+    $.ajax({
+        method: 'GET',
+        url: 'https://me.inside-out-project.com/wp-json/wp/v2/pages/' + obj,
+        dataType: 'json',
+        success: function(data) {
+            var pgbuild = '';
+            pgbuild = '<section><div class="container">' + data.content.rendered + '</div></section>';
+            $("#content").fadeOut(function() {
+                $('html').animate({
+                    scrollTop: 0
+                }, 'slow'); //IE, FF
+                $('body').animate({
+                    scrollTop: 0
+                }, 'slow'); //chrome, don't know if Safari works
+                $(this).html(pgbuild).fadeIn();
+                $("#loaderDiv").fadeOut("slow");
+            });
+        },
+        error: function() {
+            console.log('bad');
         }
-
-          theMenu = theMenu + '</li>';
-
-});
-
-    theMenu = theMenu + '</ul>';
-
-  } else {
-    console.log('no data');
-  }
-
-  return theMenu;
+    });
 }
-=======
->>>>>>> 102-1
